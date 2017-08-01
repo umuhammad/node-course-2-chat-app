3824ab6d0f34109d0958c524bee7ecdc730e58e8
Usman Muhammad <Usman@Usmans-MacBook-Air.local>
2017-08-01 13:04:14 -0400

    Scroll to bottom if user is close to bottom

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,22 @@
 var socket = io();
 
+function scrollToBottom () {
+  // Selectors
+  var messages = jQuery('#messages');
+  var newMessage = messages.children('li:last-child')
+  // Heights
+  var clientHeight = messages.prop('clientHeight');
+  var scrollTop = messages.prop('scrollTop');
+  var scrollHeight = messages.prop('scrollHeight');
+  var newMessageHeight = newMessage.innerHeight();
+  var lastMessageHeight = newMessage.prev().innerHeight();
+
+  if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight  >= scrollHeight) {
+    messages.scrollTop(scrollHeight);
+  }
+}
+
+
 socket.on('connect', function () {
   console.log('Connected to server');
 });
@@ -18,12 +35,7 @@ socket.on('newMessage', function (message) {
   });
 
   jQuery('#messages').append(html);
-
-  // console.log('newMessage', message);
-  // var li = jQuery('<li></li>');
-  // li.text(`${message.from}: ${formattedTime}: ${message.text}`);
-  //
-  // jQuery('#messages').append(li);
+  scrollToBottom();
 });
 
 socket.on('newLocationMessage', function (message) {
@@ -36,6 +48,7 @@ socket.on('newLocationMessage', function (message) {
   });
 
   jQuery('#messages').append(html);
+  scrollToBottom();
 });
 
 jQuery('#message-form').on('submit', function (e) {
